6ff400072f0f5c949fa381c1756b10639d3acfad
ahmedbalwa <ahmedbalwa2004@gmail.com>
2026-02-16 15:20:48 +0530

    Added shop page

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-function buyPlan(plan) {
-    alert(plan + " plan selected!");
+async function buyPlan(planName, price) {
+
+    const userEmail = prompt("Enter your email:");
+
+    if (!userEmail) {
+        alert("Email is required!");
+        return;
+    }
+
+    const response = await fetch("http://localhost:5000/api/buy-plan", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            email: userEmail,
+            plan: planName,
+            amount: price
+        })
+    });
+
+    const data = await response.json();
+    alert(data.message);
 }
